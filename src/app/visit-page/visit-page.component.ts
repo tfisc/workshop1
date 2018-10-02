4a5298fe24ae32c49ffe4c0d80315e17235896a3
Thibault Fischer <utilisateur@L-FR-AC02WF28EH.local>
2018-10-02 12:16:36 +0200

    routing + visit page

---
+++ src/app/visit-page/visit-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-visit-page',
+  templateUrl: './visit-page.component.html',
+  styleUrls: ['./visit-page.component.css']
+})
+export class VisitPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}

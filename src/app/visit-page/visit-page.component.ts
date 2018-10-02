34c8d2400696e633cc6820c38ba26c3de05d3e6d
joconte <etud@contejonathan.com>
2018-10-02 12:59:08 +0200

    ajout de la logique tableau de liste de piece

--- src/app/visit-page/visit-page.component.ts
+++ src/app/visit-page/visit-page.component.ts
@@ -11,7 +11,20 @@ export class VisitPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    function fait_piece(nNomPiece,nDescription,nCoordX, nCoordY) {
+      this.nomPiece = nNomPiece;
+      this.description = nDescription;
+      this.coordX = nCoordX;
+      this.coordY = nCoordY;
+    }
+  
+    var listePiece = new Array(7);
+    listePiece[0] = new fait_piece("VueBatimentCroixVerte","Le batiment croix verte est situé 1027 rue de la croix verte.",0,0);
+    listePiece[1] = new fait_piece("EntreeBatimentCroixVerte","Nous voyons ici l'entrée des artistes !",0,1);
+
   }
 
+  
 
 }
